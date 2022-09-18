15c976112e6fa8de28b9ea54e3a4edaac7c30889
Sae-byeol <toquf0424@naver.com>
2022-09-19 02:08:57 +0900

    feat: 댓글 post, delete 완료

--- src/pages/GalleryContent/Comment.jsx
+++ src/pages/GalleryContent/Comment.jsx
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import { BlackText } from '../../components/commons/Font'
 import { ColContainer, RowContainer } from '../../components/commons/Container'
+import { GrayInput } from '../../components/commons/Inputs'
+import { GrayRoundBtn } from '../../components/commons/Btns'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 const Comment = ({comments}) => {
-    console.log(comments);
+    console.log(comments)
+    const [addComment, setAddComment]=useState('');
+    const id=useParams();
+    const writeComment=(e)=>{
+        setAddComment(e.target.value);
+    }
+    const clickAdd=()=>{
+        axios.post('/api/v1/comment', 
+            {
+                commentContent: addComment,
+                exhibitionId: 1
+            }
+        )
+        .then((res)=>{console.log(res.data); window.location.reload()})
+    }
+    const clickDelete=(id)=>[
+        axios.delete(`/api/v1/comment/${id}`)
+        .then((res)=>{console.log(res.data); window.location.reload()})
+    ]
   return (
     <CommentContainer>
         <Title>
         <BlackText size="26px">comment</BlackText>
         </Title>
+        <WriteContainer>
+            <Write onChange={writeComment}></Write>
+            <Add onClick={clickAdd}>작성</Add>
+        </WriteContainer>
         <Content>
             {
-                comments.map((comment)=>(
-                <RowContainer style={{gap:"20px"}}>
+                comments.map((comment, id)=>(
+                <RowContainer style={{gap:"20px", width: "90%", justifyContent:"space-between"}}>
                     <Img src='../../imgs/profileSample.svg'/>
-                    <div>
+                    <div style={{width:"80%"}}>
                         <BlackText size="20px">{comment.user.nickname}</BlackText>
                         <BlackText size="20px">{comment.commentContent}</BlackText>
                     </div>
+                    <Delete onClick={()=>clickDelete(comment.commentId)}>
+                        삭제
+                    </Delete>
                 </RowContainer>
                 ))
             }
@@ -38,10 +67,32 @@ const Title=styled.div`
 `
 const Content=styled(ColContainer)`
     margin-top: 50px;
-    align-items: flex-start;
+    //align-items: flex-start;
     width: 80%;
     gap:40px;
 `
 const Img=styled.img`
     
+`
+const WriteContainer=styled.div`
+    margin: 30px 0;
+    width: 80%;
+    position: relative;
+`
+const Write=styled(GrayInput)`
+    width: 100%;
+    height: 100px;
+    border-radius: 30px;
+    
+`
+const Add=styled(GrayRoundBtn)`
+    position: absolute; 
+    right: 20px;
+    bottom: 20px;
+    width: 7%;
+    height: 40px;
+`
+const Delete=styled(GrayRoundBtn)`
+    width: 7%;
+    height: 40px;
 `
